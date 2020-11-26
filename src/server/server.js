e20b58b8523c26492d87af31240d29f10317516a
CharleyDvs <ing.carlosjaime@outlook.com>
2020-11-26 17:48:59 -0600

    Reordered archives to frontend and server directories, added a simple server

---
+++ src/server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('*', (req, res) => {
+    res.send({
+        hello: 'Express'
+    });
+});
+
+app.listen(3000, err => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Server running on port 3000');
+    }
+});
