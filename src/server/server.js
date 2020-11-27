4390c7fdf3ddda88d47b38f16a889ae53990564d
CharleyDvs <ing.carlosjaime@outlook.com>
2020-11-26 23:22:11 -0600

    Added webpack hot loader

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,36 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
 
+if (ENV === 'development') {
+    console.log('Development Config');
+    const webpackConfig = require('../../webpack.config');
+    const webpackDevMiddleware = require('webpack-dev-middleware');
+    const webpackHotMiddleware = require('webpack-hot-middleware');
+    const compiler = webpack(webpackConfig);
+    const { publicPath } = webpackConfig.output;
+    const serverConfig = {
+        serverSideRender: true,
+        publicPath
+    };
+
+    app.use(webpackDevMiddleware(compiler, serverConfig));
+    app.use(webpackHotMiddleware(compiler));
+}
+
 app.get('*', (req, res) => {
+    console.log('Hello')
     res.send({
         hello: 'Express'
     });
 });
 
-app.listen(3000, err => {
+app.listen(PORT, err => {
     if (err) {
         console.log(err);
     } else {
